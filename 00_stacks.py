18dbbfaa610b9514bf3ba24de51b5b10677d1cad
Mwiza MBP 2017 <mwiza@agripredict.com>
2019-02-06 04:26:38 +0200

    Stack DS implementation

---
+++ 00_stacks.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
