29a8b6687b035a69a0243fa7c40cc4c14ef9be9e
Mwiza MBP 2017 <mwiza@agripredict.com>
2019-02-19 09:55:25 +0200

    Created basic structure for LL Nodes

---
+++ 03_linked_list.py
@@ -0,0 +1,12 @@
+class Node:
+    def __int__(self, data):
+        self.data = data # Assign the data here
+        self.next = None # Set next to None by default.
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+# Tests
+
