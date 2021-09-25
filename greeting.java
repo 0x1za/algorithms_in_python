095cb143499cc206a9985227133607a312365a49
Mwiza Simbeye <mwiza@agripredict.com>
2021-09-25 15:31:33 +0200

    Create greeting.java
    This is a basic calculator

---
+++ greeting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class basicCaluculator{
+  public static void main(String[] args) {
+    int firstNum;
+    int secondNum;
+    answer = firstNum + secondNum;
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your number here: ");
+    input.nextInt();
+    
+    System.out.println("Enter your second number here: ");
+    input.nextInt();
+    
+    System.out.println(answer);
+    
+  } 
+}
